a7dca19a9a2e014e1bd4f7850f4ee27c8b0d5d5c
bharatbattina <bharathbattina@gmail.com>
2022-02-15 23:36:25 -0500

    first commit

---
+++ src/main/java/myproject/com/App.java
@@ -0,0 +1,13 @@
+package myproject.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
